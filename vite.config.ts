b1d7f5dccbef86883cfc80f6618bb6839643420b
keer4n <keer4n@protonmail.com>
2024-02-25 20:18:43 -0700

    add base in service worker

--- vite.config.ts
+++ vite.config.ts
@@ -19,19 +19,19 @@ export default defineConfig({
       manifest: {
         short_name: 'Fmap',
         name: 'Fmap',
-        start_url: '/',
-        scope: '/',
+        start_url: '/fmap',
+        scope: '/fmap/',
         display: 'standalone',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         icons: [
           {
-            src: '/logo-192x192.png',
+            src: './logo-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/logo-512x512.png',
+            src: './logo-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
