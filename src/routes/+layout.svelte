af067625ac6b515aa0add79027dbcc25b4927574
keer4n <keer4n@protonmail.com>
2024-02-25 17:48:08 -0700

    add sw

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { pwaInfo } from 'virtual:pwa-info';
+	import { pwaAssetsHead } from 'virtual:pwa-assets/head';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					console.log(`SW registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+</script>
+
+<svelte:head>
+	<title>FMAP</title>
+	{@html webManifestLink}
+	{#if pwaAssetsHead.themeColor}
+		<meta name="theme-color" content={pwaAssetsHead.themeColor.content} />
+	{/if}
+	{#each pwaAssetsHead.links as link}
+		<link {...link} />
+	{/each}
+</svelte:head>
+
+<slot />
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,53 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import {SvelteKitPWA} from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  define: {
+    'process.env.NODE_ENV' : process.env.NODE_ENV === 'production' ? '"production"' : '"development"'
+  },
+  plugins: [sveltekit(),
+    SvelteKitPWA({
+      srcDir: './src',
+      mode: 'development',
+      strategies: 'injectManifest',
+      filename: 'service-worker/index.ts',
+      scope: '/',
+      base: '/',
+      selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
+      manifest: {
+        short_name: 'Fmap',
+        name: 'Fmap',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+
+      },
+      injectManifest: {
+        globPatterns: ['client/**/*.{js,css,png}']
+      },
+      workbox: {
+        globPatterns: ['client/**/*.{js,css,png}']
+      },
+      devOptions: {
+        enabled: true,
+        suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
+        type: 'module',
+        navigateFallback: '/'
+      }
+    })]
 });
